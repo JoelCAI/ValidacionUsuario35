731283f132b75d10a6098022681b687b2402722a
JoelCAI <joelargentina12345@gmail.com>
2022-04-15 11:51:56 -0300

    Agregar archivos de proyecto.

---
+++ ValidacionUsuario35/Sistema.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValidacionUsuario35
+{
+    public class Sistema
+    {
+        public void MenuPrincipal()
+        {
+            Validador.Bienvenida();
+
+            int numero = Validador.PedirInt("\n Ingrese un número");
+
+            Console.Clear();
+            Console.WriteLine("\n El número ingresado fue *" + numero + "*.");
+
+            Validador.Despedida();
+
+        }
+       
+        public void Iniciar()
+        {
+            MenuPrincipal();
+        }
+
+
+    }
+}
---
+++ ValidacionUsuario35/Validador.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValidacionUsuario35
+{
+    public class Validador
+    {
+        public static int PedirInt(string mensaje)
+        {
+            /* la salida de esta funcion estará en la variable int valor */
+            string mensError = "\n\n Por favor ingrese el valor solicitado" +
+                               "\n Debe ingresar un valor que no sea vacio y que ese valor sea un número. ";
+
+            int valor;
+            Console.WriteLine(mensaje);
+
+            if (!int.TryParse(Console.ReadLine(), out valor))
+            {
+                Console.Clear();
+                Console.WriteLine(mensError);
+                return PedirInt(mensaje);
+            }
+            else
+                return valor;
+        }
+
+        public static void Bienvenida()
+        {
+            Console.WriteLine("\n Bienvenido al Programa");
+        }
+
+        public static void Despedida()
+        {
+
+            Console.WriteLine("\n Gracias por usar nuestro Sistema presione cualquier teclar para finalizar");
+            Console.ReadKey();
+        }
+    }
+}
